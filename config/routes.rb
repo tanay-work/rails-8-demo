06c57a44ea7fdbc0a0c5ce605ef477e2c65d3cfb
Tanay <tanaywork21@gmail.com>
2025-02-07 21:47:45 +0530

    add index product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
+  root "products#index"
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/products", to: "products#index"
-  
+  resources :products
+
 end
