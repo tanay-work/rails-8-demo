6fbf1d61b655aadc7d976d678fb4449b1414c19d
Tanay <tanaywork21@gmail.com>
2025-02-08 19:02:24 +0530

    demo with kamal deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
 
   root "products#index"
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :products
-
+  resources :products do
+    resources :subscribers, only: [ :create ]
+    resource :unsubscribe, only: [ :show ]
+  end
 end
